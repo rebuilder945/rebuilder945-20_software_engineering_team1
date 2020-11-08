9795a830127c913cd90ef54b2c066f9e5c660052
郑锐威 <70941867+Kingjizrw@users.noreply.github.com>
2020-11-08 20:07:51 +0800

    Add files via upload

---
+++ 郑锐威/show_group.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import mysql.connector
+import pymysql
+import json,os,sys,getopt,argparse
+
+#显示组的投票信息
+
+# 内部接口：
+# 从前端接收的：
+# {
+#     "user_id":"00001"
+# }
+# 返回的：
+# {
+#     "status":true,
+#     "group_form":{
+#         {
+#             "group_id":"1",
+#             "vote_num":"0"
+#         },
+#         {
+#             "group_id":"2",
+#             "vote_num":"1"
+#         }
+#     }
+# }
+# {
+#     "status":false,
+#     "msg":"?"
+# }
+
+
+jsonmessage = "{\"user_id\":\"00000\"}"
+
+tmp = json.loads(jsonmessage)
+
+
+mydb = pymysql.connect("localhost","root","","vote_system" )
+gcursor = mydb.cursor()
+
+gcursor.execute("SELECT group_id,vote_num FROM group_info")
+result = gcursor.fetchall()
+
+ret = "{\"status\":\"true\",\"group_form\":{"
+
+
+group_form = {'group_id':'','vote_num':''}
+
+for i in result:
+    if i[0] != 1:
+        ret += ","
+    group_form['group_id'] = i[0]
+    group_form['vote_num'] = i[1]
+    ret += json.dumps(group_form)
+
+ret += "}}"
+#模拟信息回送
+print(ret)
